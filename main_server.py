5df9e46c6d1840f59a7f5e10b230c9d6038f90f2
smooth <68108963+smoothwastaken@users.noreply.github.com>
2023-03-19 12:27:28 +0100

    [general]: Added more communication between main server and client servers.

--- main_server.py
+++ main_server.py
@@ -49,7 +49,7 @@ class MainServer():
         self.format = self.config["format"]
 
         # Set verbose variable
-        self.verbose = self.config["verbose"]
+        self.verbose_logs = self.config["verbose"]
 
     def create_socket(self):
         """Creates a socket and binds it to the provided host and port.
@@ -57,29 +57,25 @@ class MainServer():
         # Create socket
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind((self.host, self.port))
-        if self.verbose:
-            print(
-                f"Socket binded to the provided host ({self.host}) and port ({self.port}).")
+        self.verbose(
+            f"Socket binded to the provided host ({self.host}) and port ({self.port}).")
 
     def listen(self):
         """Listens for incoming connections.
         """
         # Start listening
         self.socket.listen(self.max_connections)
-        if self.verbose:
-            print(
-                f"Socket started listening with a maximum of {self.max_connections} connections.")
+        self.verbose(
+            f"Socket started listening with a maximum of {self.max_connections} connections.")
 
         # Accept connections
         while True:
             client_server, address = self.socket.accept()
-            if self.verbose:
-                print(
-                    f"Connection from server client {address[0]}:{address[1]} has been established.")
+            self.verbose(
+                f"Connection from server client {address[0]}:{address[1]} has been established.")
 
             # Start a new thread for each client
-            if self.verbose:
-                print("Starting new thread for the client server...")
+            self.verbose("Starting new thread for the client server...")
             start_new_thread(self.handle_client_servers,
                              (client_server, address,))
 
@@ -90,18 +86,65 @@ class MainServer():
             client (): Client server object.
             address (list): Address of the client server.
         """
-        message = self.handle_client_message(client)
-        print(message)
-        self.send(client, "#CONNECTED")
-        client.close()
+        connected = False
+        # Waiting for the client server to send a register request
+        register_request = self.handle_client_message(client)
+
+        # Registering the client server
+        if "$REGISTER" in register_request:
+            self.verbose(
+                f"Received register request from client server with id: '{register_request.split(' ')[1]}'.")
+
+            # Verify if the client server id is valid
+            id, valid_id = self.verify_client_id(
+                register_request.split(' ')[1])
+            if not valid_id:  # Send an error to the client server
+                self.send(client, "#!INVALID_ID")
+                self.close(client, address)
+                return
+            else:  # Send a confirmation to the client server
+                self.send(client, "#VALID_ID")
+
+            # Send the client server a request for its name
+            self.send(client, "?NAME")
+            # Receive the client server name
+            client_server_name = self.handle_client_message(client)
+            # Send the client server a confirmation
+            self.send(client, "#VALID_NAME")
+
+            # Send the client server registration confirmation
+            self.send(client, "#REGISTERED @" + client_server_name)
+            connected = True
+            while connected:
+                break
+
+        self.close(client, address)
+
+    def verbose(self, msg: str):
+        if self.verbose_logs:
+            print(msg)
 
     def send(self, client, msg: str):
+        """Sends a message to the client.
+
+        Args:
+            client (object): Client server object.
+            msg (str): Message to send.
+        """
         message = msg.encode(self.format)
         msg_length = len(message)
         send_length = str(msg_length).encode(self.format)
         send_length += b' ' * (self.header - len(send_length))
         client.send(send_length)
         client.send(message)
+
+    def close(self, client, address):
+        self.send(client, "#CLOSE")
+        self.verbose(
+            f"Closing connection for client server {address[0]}:{address[1]}...")
+        client.close()
+        self.verbose(
+            f"Connection for client server {address[0]}:{address[1]} closed.")
 
     def handle_client_message(self, client):
         msg_length = client.recv(self.header).decode(self.format)
@@ -110,6 +153,29 @@ class MainServer():
             data = client.recv(msg_length)
             msg = data.decode(self.format)
             return msg
+
+    def verify_client_id(self, id):
+        # Verify id's length
+        self.verbose(f"Verifying client server id: '{id}'...")
+        self.verbose(f"Verifying the length of the id...")
+        if len(id) != 40:
+            self.verbose(
+                f"The id's length is not valid. (20 characters required, {len(id)} provided)")
+            return id, False
+        self.verbose(f"The id's length is valid.")
+
+        # Verify id's characters
+        self.verbose(f"Verifying the characters of the id...")
+        authorized_chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        i = 0
+        for char in id:
+            if char not in authorized_chars:
+                self.verbose(f"The id contains an unauthorized character: '{char}' at index {i}.")
+                return id, False
+            i += 1
+        self.verbose(f"The id's characters are valid.")
+        self.verbose(f"Client server id verified.")
+        return id, True
 
 
 if __name__ == "__main__":
--- server/server.py
+++ server/server.py
@@ -48,12 +48,19 @@ class Server():
         self.main_server_host = self.config["main_server"]["host"]
         self.main_server_port = int(self.config["main_server"]["port"])
 
+        # Set local server variables
+        self.server_settings = self.config["server_settings"]
+        self.server_id = self.server_settings["id"]
+        self.server_name = self.server_settings["name"]
+        self.server_description = self.server_settings["description"]
+        self.server_max_clients = self.server_settings["max_clients"]
+
         # Set message variables
         self.header = self.config["header"]
         self.format = self.config["format"]
 
         # Set verbose variable
-        self.verbose = self.config["verbose"]
+        self.verbose_logs = self.config["verbose"]
 
     def create_socket(self):
         """Creates a socket and binds it to the provided host and port.
@@ -66,32 +73,60 @@ class Server():
         """
         # Bind socket
         self.socket.bind((self.host, self.port))
-        if self.verbose:
-            print(
-                f"Socket binded to the provided host ({self.host}) and port ({self.port}).")
+        self.verbose(
+            f"Socket binded to the provided host ({self.host}) and port ({self.port}).")
 
     def register(self):
         """Registers the server to the main server.
         """
         self.socket.connect((self.main_server_host, self.main_server_port))
-        self.send("register")
-        print(self.handle_main_server_message())
+        self.send(f"$REGISTER {self.server_id}")
+        id_status = self.handle_main_server_message()
+        if id_status == "#!INVALID_ID":
+            self.verbose("The provided ID is invalid.")
+            self.socket.close()
+            exit()
+        # elif id_status == "#!ID_ALREADY_IN_USE":
+        #     self.verbose("The provided ID is already in use.")
+        #     self.socket.close()
+        #     exit()
+        else:
+            main_server_request = self.handle_main_server_message()
+            if main_server_request != "?NAME":
+                self.socket.close()
+                return
+
+            self.send(f"$NAME {self.server_name}")
+            name_status = self.handle_main_server_message()
+            if name_status == "#!INVALID_NAME":
+                self.verbose("The provided name is invalid.")
+                self.socket.close()
+                exit()
+            # elif name_status == "#!NAME_ALREADY_IN_USE":
+            #     self.verbose("The provided name is already in use.")
+            #     self.socket.close()
+            #     exit()
+            else:
+                main_server_request = self.handle_main_server_message()
+                if '#REGISTERED' not in main_server_request:
+                    self.verbose("Server registration failed.")
+                    self.socket.close()
+                    exit()
+                self.verbose("Server registered successfully.")
 
     def listen(self):
         """Listens for incoming connections.
         """
         # Start listening
         self.socket.listen(self.max_connections)
-        if self.verbose:
-            print(
-                f"Socket started listening with a maximum of {self.max_connections} connections.")
+        self.verbose(
+            f"Socket started listening with a maximum of {self.max_connections} connections.")
 
         # Accept connections
         while True:
             client, address = self.socket.accept()
-            if self.verbose:
-                print(
-                    f"Connection from client {address[0]}:{address[1]} has been established.")
+            self.verbose(
+                f"Connection from client {address[0]}:{address[1]} has been established.")
 
             # Start a new thread for each client
             start_new_thread(self.handle_clients, (client,))
@@ -104,6 +139,10 @@ class Server():
             address (list): Address of the client server.
         """
         print(client)
+
+    def verbose(self, msg: str):
+        if self.verbose_logs:
+            print(msg)
 
     def send(self, msg: str):
         """Send a message to a client.
